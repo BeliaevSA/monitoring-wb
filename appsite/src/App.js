32949365d217ca277c0b20f6335db7bd0590d8f5
Beliaev Sergei <beliaevsa1@gmail.com>
2026-05-06 02:13:18 +0300

    06,05,26 2,13

--- appsite/src/App.js
+++ appsite/src/App.js
@@ -1,14 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppSite from "./AppSite";
-// import PartnerPresentation from "./PartnerPresentation";
-
+import ManagerPresentation from "./ManagerPresentation";
+import PartnerPresentation from "./PartnerPresentation";
+ 
+// Роутинг по домену — один контейнер, три страницы
+const ROUTES = {
+  // мониторинг-вб.рф
+  "xn----btbccya0aicgcyy.xn--p1ai":        AppSite,
+  "www.xn----btbccya0aicgcyy.xn--p1ai":    AppSite,
+ 
+  // менеджерам.мониторинг-вб.рф
+  "xn--80ahcabf2bgd8a.xn----btbccya0aicgcyy.xn--p1ai":      ManagerPresentation,
+  "www.xn--80ahcabf2bgd8a.xn----btbccya0aicgcyy.xn--p1ai":  ManagerPresentation,
+ 
+  // партнерам.мониторинг-вб.рф
+  "xn--80aao3ackicp.xn----btbccya0aicgcyy.xn--p1ai":        PartnerPresentation,
+  "www.xn--80aao3ackicp.xn----btbccya0aicgcyy.xn--p1ai":    PartnerPresentation,
+};
+ 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppSite />} />
-        {/* <Route path="/partners" element={<PartnerPresentation />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
+  const host = window.location.hostname;
+  const Component = ROUTES[host] ?? AppSite;
+  return <Component />;
 }
+ 
